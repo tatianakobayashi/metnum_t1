cb4c67859310e666cd84530a9fd065c037a4095b
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-09-18 07:39:30 -0400

    Update bisseccao_e_newton.py

--- bisseccao_e_newton.py
+++ bisseccao_e_newton.py
@@ -1,62 +1,34 @@
-from __future__ import division  
- 
 # Bissecção
-def bissecao(f, a, b, TOL, N):  
+def bissecao(f, a, b, parada, N):  
     i = 1  
-    fa = f(a)  
+    p = 0
     while (i <= N):  
-        #iteracao da bissecao  
-        p = a + (b-a)/2  
-        fp = f(p)  
-        #condicao de parada  
-        if ((fp == 0) or ((b-a)/2 < TOL)):  
-            return p  
-        #bissecta o intervalo  
-        i = i+1  
-        if (fa * fp > 0):  
-            a = p  
-            fa = fp  
-        else:  
-            b = p  
- 
-    raise NameError(’Num. max. de iter. excedido!’);
+        fa = f(a)  
+
+        p = (a + b)/2  
+        fp = f(p) 
+
+        fb = f(b)
+
+        if (fa == 0):
+            return a
+        elif (fp == 0 or abs(b - a) <= parada):
+            return p
+        elif (fb == 0):
+            return b
+
+        if (fa > 0 and fp < 0) or (fa < 0 and fp < 0):
+            b = p
+        elif (fb > 0 and fp < 0) or (fb < 0 and fp < 0):
+            a = p
+        i += 1  
+    return p
 
 # Newton
-def F(x):  
-    y = np.zeros(2)  
- 
-    y[0] = x[0]**2 - np.cos(x[0]*x[1]) - 1  
-    y[1] = np.sin(x[1]) - 2*np.cos(x[0])  
- 
-    return y  
- 
-def JF(x):  
-    y = np.zeros((2,2))  
- 
-    y[0,0] = 2*x[0] + x[1]*np.sin(x[0]*x[1])  
-    y[0,1] = x[0]*np.sin(x[0]*x[1])  
- 
-    y[1,0] =  2*np.sin(x[0])  
-    y[1,1] = np.cos(x[1])  
- 
-    return y
-
-from __future__ import division  
-import numpy as np  
-from numpy import linalg  
- 
-def newton(F,JF,x0,TOL,N):  
-    #preliminares  
-    x = np.copy(x0).astype(’double’)  
-    k=0  
-    #iteracoes  
-    while (k < N):  
-       k += 1  
-       #iteracao Newton  
-       delta = -np.linalg.inv(JF(x)).dot(F(x))  
-       x = x + delta  
-       #criterio de parada  
-       if (np.linalg.norm(delta,np.inf) < TOL):  
-           return x  
- 
-    raise NameError(’num. max. iter. excedido.’)
+def newton(f, f_x0, N, E):  
+    x = [0 for i in range(N)]
+    x[0] = x0
+    for k in range(0, N):
+        x[k + 1] = x[k] - (f(x[k])/f_(x[k]))
+        if(abs(x[k+1]-x[k])/abs(x[k+1]) <= E and abs(f(x[k+1])) <= E):
+            return x[k+1]
